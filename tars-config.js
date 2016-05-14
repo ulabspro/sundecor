c3bc3e79722ebe8150bb9282fba8f766092f9f40
extiser <1sappho@gmail.com>
2016-05-14 15:57:36 +0500

    init tars project

---
+++ tars-config.js
@@ -0,0 +1,72 @@
+module.exports = {
+    "autoprefixerConfig": [
+        "> 1%",
+        "last 2 versions",
+        "Firefox ESR",
+        "android 4"
+    ],
+    "postcss": [],
+    "svg": {
+        "active": true,
+        "workflow": "sprite",
+        "symbolsConfig": {
+            "loadingType": "inject",
+            "usePolyfillForExternalSymbols": true,
+            "pathToExternalSymbolsFile": ""
+        }
+    },
+    "js": {
+        "workflow": "concat",
+        "bundler": "webpack",
+        "lint": false,
+        "useBabel": false,
+        "removeConsoleLog": true,
+        "webpack": {
+            "useHMR": false
+        },
+        "jsPathsToConcatBeforeModulesJs": [],
+        "lintJsCodeBeforeModules": false,
+        "jsPathsToConcatAfterModulesJs": [],
+        "lintJsCodeAfterModules": false
+    },
+    "sourcemaps": {
+        "js": {
+            "active": true,
+            "inline": true
+        },
+        "css": {
+            "active": true,
+            "inline": true
+        }
+    },
+    "notifyConfig": {
+        "useNotify": true,
+        "title": "TARS notification",
+        "sounds": {},
+        "taskFinishedText": "Task finished at: "
+    },
+    "browserSyncConfig": {
+        "baseDir": "./dev",
+        "port": 3004,
+        "open": true,
+        "browser": "default",
+        "startUrl": "/index.html",
+        "useNotifyInBrowser": true,
+        "injectChanges": false
+    },
+    "minifyHtml": false,
+    "staticPrefix": "static/",
+    "buildPath": "./builds/",
+    "useBuildVersioning": false,
+    "useArchiver": false,
+    "ulimit": 4096,
+    "templater": "jade",
+    "cssPreprocessor": "scss",
+    "useImagesForDisplayWithDpi": [
+        96
+    ],
+    "fs": {
+        "staticFolderName": "static",
+        "imagesFolderName": "images"
+    }
+};
